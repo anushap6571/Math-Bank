1ed46e0526fa5676815d3e7dcf78267af8dfdfb8
DAJimenez4 <157992985+DAJimenez4@users.noreply.github.com>
2024-11-27 14:58:29 -0600

    Fix error for login
    Added a body for username and password retrieval (forgotten in merge).

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -99,6 +99,11 @@ const LogInPage = ({setIsLoggedIn, setUsername}) => {
     const submitLogIn = (logInData) => {
         console.log(logInData);
 
+        const body = {
+            username: logInData.username,
+            password: logInData.password
+        }
+
         fetch('http://127.0.0.1:5000/logInReq', {
             method: 'POST',
             headers: {
